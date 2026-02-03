2b8d4eb0f91ef24b0d58aa6ce6fc376b9e55434d
Poorna Pradeep <pradeep.p@sharviinfotech.com>
2026-02-03 20:44:33 +0530

    All Screens Css Changed

--- src/app/pages/dashboards/invoice-load-details/invoice-load-details.component.ts
+++ src/app/pages/dashboards/invoice-load-details/invoice-load-details.component.ts
@@ -1169,6 +1169,7 @@ export class InvoiceLoadDetailsComponent implements OnInit {
 
 
           Swal.fire('Data fetched successfully!', '', 'success');
+          this.cd.detectChanges();
         }
       },
       error: (err) => {
